508068baf301bd2a626972bc53590caaadbd2546
PoTa <pota@mosfet.hu>
2018-09-18 13:17:57 +0200

    add html anchors, fixes #31

--- src/templates.js
+++ src/templates.js
@@ -96,6 +96,9 @@ module.exports = {
       if (options.anchors) {
         return '{#' + name + '}';
       }
+      else if (options.htmlAnchors) {
+        return '<a id="' + name + '"></a>';
+      }
       else {
         return '';
       }

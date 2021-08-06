17de5378a43e09a901358ad548adcd47df534e56
Sebastian Seifert <seifert.sebastian@siemens.com>
2021-08-06 14:31:30 +0200

    Treat interface compounds (e.g. keyword "interface" in C#) like classes.

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ module.exports = {
         'struct',
         'union',
         'typedef',
+        'interface',
         // 'file'
       ]
     }
--- src/templates.js
+++ src/templates.js
@@ -55,6 +55,7 @@ module.exports = {
         break;
       case 'class':
       case 'struct':
+      case 'interface':
         template = 'class';
         break;
       default:
